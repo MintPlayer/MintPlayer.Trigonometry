4c8fdc5b376ceb68ab54ee3912a7f767936a57ce
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2020-10-16 14:57:41 +0200

    Added header markups

--- MintPlayer.Trigonometry/Math.cs
+++ MintPlayer.Trigonometry/Math.cs
@@ -2,54 +2,90 @@
 {
     public static class Math
     {
-        /// <summary>Inverse hyperbolic sine</summary>
-        /// <param name="d"></param>
-        /// <returns></returns>
+        /// <summary>Returns the cosecant of the specified angle.</summary>
+        /// <param name="d">An angle, measured in radians.</param>
+        public static double Cosec(double d)
+        {
+            return 1 / System.Math.Sin(d);
+        }
+
+        /// <summary>Returns the secant of the specified angle.</summary>
+        /// <param name="d">An angle, measured in radians.</param>
+        public static double Sec(double d)
+        {
+            return 1 / System.Math.Cos(d);
+        }
+
+        /// <summary>Returns the cotangent of the specified angle.</summary>
+        /// <param name="d">An angle, measured in radians.</param>
+        public static double Cotan(double d)
+        {
+            return 1 / System.Math.Tan(d);
+        }
+
+        /// <summary>Returns the hyperbolic cosecant of the specified angle.</summary>
+        /// <param name="d">An angle, measured in radians.</param>
+        public static double Cosech(double d)
+        {
+            return 1 / System.Math.Sinh(d);
+        }
+
+        /// <summary>Returns the hyperbolic secant of the specified angle.</summary>
+        /// <param name="d">An angle, measured in radians.</param>
+        public static double Sech(double d)
+        {
+            return 1 / System.Math.Cosh(d);
+        }
+
+        /// <summary>Returns the hyperbolic cotangent of the specified angle.</summary>
+        /// <param name="d">An angle, measured in radians.</param>
+        public static double Cotanh(double d)
+        {
+            return 1 / System.Math.Tanh(d);
+        }
+
+        /// <summary>Returns the angle whose hyperbolic sine is the specified number.</summary>
+        /// <param name="d">A number representing a hyperbolic sine.</param>
         public static double AsinH(double d)
         {
             var asinh = System.Math.Log(d + System.Math.Sqrt(1 + System.Math.Pow(d, 2)));
             return asinh;
         }
 
-        /// <summary>Inverse hyperbolic cosine</summary>
-        /// <param name="d"></param>
-        /// <returns></returns>
+        /// <summary>Returns the angle whose hyperbolic cosine is the specified number.</summary>
+        /// <param name="d">A number representing a hyperbolic cosine.</param>
         public static double AcosH(double d)
         {
             var acosh = System.Math.Log(d + System.Math.Sqrt(d + 1) * System.Math.Sqrt(d - 1));
             return acosh;
         }
 
-        /// <summary>Inverse hyperbolic tangent</summary>
-        /// <param name="d"></param>
-        /// <returns></returns>
+        /// <summary>Returns the angle whose hyperbolic tangent is the specified number.</summary>
+        /// <param name="d">A number representing a hyperbolic tangent.</param>
         public static double AtanH(double d)
         {
             var atanh = System.Math.Log((1 + d) / (1 - d)) / 2;
             return atanh;
         }
 
-        /// <summary>Inverse hyperbolic cosecant</summary>
-        /// <param name="d"></param>
-        /// <returns></returns>
+        /// <summary>Returns the angle whose hyperbolic cosecant is the specified number.</summary>
+        /// <param name="d">A number representing a hyperbolic cosecant.</param>
         public static double AcosecH(double d)
         {
             var acosech = System.Math.Log(1 / d + System.Math.Sqrt(1 / System.Math.Pow(d, 2) + 1));
             return acosech;
         }
 
-        /// <summary>Inverse hyperbolic secant</summary>
-        /// <param name="d"></param>
-        /// <returns></returns>
+        /// <summary>Returns the angle whose hyperbolic secant is the specified number.</summary>
+        /// <param name="d">A number representing a hyperbolic secant.</param>
         public static double AsecH(double d)
         {
             var asech = System.Math.Log(1 / d + System.Math.Sqrt(1 / d + 1) * System.Math.Sqrt(1 / d - 1));
             return asech;
         }
 
-        /// <summary>Inverse hyperbolic contangent</summary>
-        /// <param name="d"></param>
-        /// <returns></returns>
+        /// <summary>Returns the angle whose hyperbolic contangent is the specified number.</summary>
+        /// <param name="d">A number representing a hyperbolic contangent.</param>
         public static double AcotanH(double d)
         {
             var acotanh = System.Math.Log((d + 1) / (d - 1)) / 2;
